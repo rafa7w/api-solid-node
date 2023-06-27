b1284bcb928808b4f1748c6be2924e8889c04261
Rafael William <rafael.fim@hotmail.com>
2023-06-27 09:59:07 -0300

    test: password hash

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,11 +1,16 @@
 import { hash } from 'bcryptjs';
 import { UsersRepository } from '@/repositories/users-repository';
 import { UserAlreadyExistsError } from './errors/user-already-exists-error';
+import { User } from '@prisma/client';
 
 interface RegisterUserCaseRequest {
   name: string;
   email: string;
   password: string;
+}
+
+interface RegisterUseCaseResponse {
+  user: User;
 }
 
 // Aplicação de Inversão de Dependência (SOLID)
@@ -14,7 +19,7 @@ export class RegisterUseCase {
     this.usersRepository = usersRepository
   }
 
-  async execute({name, email, password}: RegisterUserCaseRequest) {
+  async execute({name, email, password}: RegisterUserCaseRequest): Promise<RegisterUseCaseResponse> {
     // o segundo parâmetro passado para o hash é a quantidade de vezes que ele vai encriptografar cumulativamente a senha, ou seja, ele encriptogrfa a criptografia já feita 
     const password_hash = await hash(password, 6)
 
@@ -24,10 +29,12 @@ export class RegisterUseCase {
       throw new UserAlreadyExistsError()
     }
   
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email, 
       password_hash
     })
+
+    return {user}
   }
 }
