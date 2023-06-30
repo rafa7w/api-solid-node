894cb138955fe695cc4d01ab2324052ebfa4c1d3
Rafael William <rafael.fim@hotmail.com>
2023-06-30 15:01:39 -0300

    implement TDD and mocking

--- src/use-cases/check-in.ts
+++ src/use-cases/check-in.ts
@@ -1,9 +1,13 @@
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
+import { GymsRepository } from "@/repositories/gyms-repository";
 import { CheckIn } from "@prisma/client";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
 
 interface CheckInUseCaseRequest {
   userId: string;
   gymId: string;
+  userLatitude: number;
+  userLonigtude: number;
 }
 
 interface CheckInUseCaseResponse {
@@ -11,10 +15,28 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {
-  }
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository
+  ) {}
 
   async execute({userId, gymId}: CheckInUseCaseRequest): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    // calcular a distância entre o usuário e a academia
+    // se > 100m = Erro de distância inválida
+    
+
+    const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(userId, new Date())
+
+    if (checkInOnSameDate) {
+      throw new Error()
+    }
+
     const checkIn = await this.checkInsRepository.create({
       gym_id: gymId,
       user_id: userId
