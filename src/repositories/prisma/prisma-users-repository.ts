e696c762c874a08d2fa569950ae7f7de4ca23240
Rafael William <rafael.fim@hotmail.com>
2023-07-07 13:47:02 -0300

    prisma: check-in methods

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -4,6 +4,16 @@ import { UsersRepository } from "../users-repository";
 
 // métodos que vão interceptar para qualquer operação no banco de dados
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      }
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     // findUnique só consegue buscar registros que foram definidos como @unique ou são chaves primárias @id
     const user = await prisma.user.findUnique({
